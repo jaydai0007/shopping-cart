a1bb637a59eae292418f5070f198c6e1120dd79d
Junjie Dai <jaydai0007@gmail.com>
2022-11-26 20:35:06 +1100

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="HomeView">
+    <h1>Browse Our Selection</h1>
+    <ProductDescriptionDrawer
+      :product="product" 
+      :active="active"
+      v-on:closeProductDrawer="closeProductDrawer()" />
+    <div class="product-cards-container">
+      <ProductSummaryCard
+        v-for="(item, i) in items"
+        :key="i"
+        :product="item"
+        v-on:view-product="viewProduct($event)" />
+    </div>    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import items from '../data/items.js';
+import ProductSummaryCard from '../components/products/ProductSummaryCard.vue'
+import ProductDescriptionDrawer from '../components/products/ProductDescriptionDrawer.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HomeView",
+  components: {ProductSummaryCard,ProductDescriptionDrawer},
+  data() {
+    return {
+      items: items,
+      product: null,
+      active: {
+        product_drawer: false
+      }
+    }
+  },
+  methods: {
+    viewProduct(product) {
+      this.product = product
+      this.active.product_drawer = true
+    },
+    closeProductDrawer() {
+      this.active.product_drawer = false
+      
+    }
   }
-}
+};
 </script>
+
+<style lang="scss">
+.product-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/views/ShopCart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Your cart</h1>
+
+        <CartItemCard 
+            v-for="product in products"
+            :key="product.id"
+            :product="product" />
+
+        <CartSummaryPaymentCard />
+
+    </div>
+</template>
+
+<script>
+import CartItemCard from '../components/ShopCart/CartItemCard.vue';
+import CartSummaryPaymentCard from '../components/ShopCart/CartSummaryPaymentCart.vue'
+
+
+
+
+export default {
+    computed: {
+        products() {
+            return this.$store.getters.cartItems;
+        }
+    },
+    components: { CartItemCard, CartSummaryPaymentCard }
+}
+</script>
